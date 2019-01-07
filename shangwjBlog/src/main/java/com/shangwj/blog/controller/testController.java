fef3325768de50e4e7462596f7dfef5dee537d7b
1056438464 <1056438464@qq.com>
2019-01-07 22:24:48 +0800

    integration redis

--- shangwjBlog/src/main/java/com/shangwj/blog/controller/testController.java
+++ shangwjBlog/src/main/java/com/shangwj/blog/controller/testController.java
@@ -1,5 +1,7 @@
 package com.shangwj.blog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "test")
 public class testController {
 
+    @Autowired
+    public RedisTemplate<Object,Object> redisTemplate;
     @RequestMapping(value = "/test1")
     public String getTest(){
         return "sssss";

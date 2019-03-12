3c8b3006d25362aea7bfdd4a95320524ccc204e7
1056438464 <1056438464@qq.com>
2019-03-12 23:04:32 +0800

    事务管理和mapper自动扫描

--- shangwjBlog/src/main/java/com/shangwj/blog/BlogApplication.java
+++ shangwjBlog/src/main/java/com/shangwj/blog/BlogApplication.java
@@ -1,11 +1,15 @@
 package com.shangwj.blog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableCaching
+@EnableTransactionManagement
+@MapperScan(basePackages="com.bjpowernode.springboot.mapper") //自动扫描mapperr
 public class BlogApplication {
 
 	public static void main(String[] args) {

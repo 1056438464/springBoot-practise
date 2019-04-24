388874927a171b4e4730541c9444b8f9d08f48b1
1056438464 <1056438464@qq.com>
2019-04-24 22:47:59 +0800

    dubbo providors

---
+++ dubbo/dubbo-service-user-provider/src/main/java/com/swj/dubboserviceuserprovider/DubboServiceUserProviderApplication.java
@@ -0,0 +1,13 @@
+package com.swj.dubboserviceuserprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboServiceUserProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubboServiceUserProviderApplication.class, args);
+	}
+
+}

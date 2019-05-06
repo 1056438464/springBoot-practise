b2c3ffa0e2f0dc40019e06a095a5be08acd161e0
1056438464 <1056438464@qq.com>
2019-05-06 23:19:39 +0800

    the first dubbo

--- dubbo/dubbo-service-user-provider/src/main/java/com/swj/dubboserviceuserprovider/api/impl/UserServlceImpl.java
+++ dubbo/dubbo-service-user-provider/src/main/java/com/swj/dubboserviceuserprovider/api/impl/UserServlceImpl.java
@@ -1,4 +1,10 @@
 package com.swj.dubboserviceuserprovider.api.impl;
 
+import com.swj.dubbo.service.user.api.userService;
+
 public class UserServlceImpl implements userService {
+    @Override
+    public String sayHi() {
+        return null;
+    }
 }

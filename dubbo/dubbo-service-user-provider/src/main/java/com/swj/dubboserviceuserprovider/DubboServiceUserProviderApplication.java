524ba33f2364c5680c8dda1c157670725a67f821
1056438464 <1056438464@qq.com>
2019-05-21 23:32:21 +0800

    dubbo

--- dubbo/dubbo-service-user-provider/src/main/java/com/swj/dubboserviceuserprovider/DubboServiceUserProviderApplication.java
+++ dubbo/dubbo-service-user-provider/src/main/java/com/swj/dubboserviceuserprovider/DubboServiceUserProviderApplication.java
@@ -1,5 +1,6 @@
 package com.swj.dubboserviceuserprovider;
 
+import com.alibaba.dubbo.container.Main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DubboServiceUserProviderApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DubboServiceUserProviderApplication.class, args);
+		Main.main(args);
 	}
 
 }
--- dubbo/dubbo-service-user-provider/src/main/java/com/swj/dubboserviceuserprovider/api/impl/UserServlceImpl.java
+++ dubbo/dubbo-service-user-provider/src/main/java/com/swj/dubboserviceuserprovider/api/impl/UserServlceImpl.java
@@ -1,10 +1,12 @@
 package com.swj.dubboserviceuserprovider.api.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.swj.dubbo.service.user.api.userService;
 
+@Service(version = "1.0.0")
 public class UserServlceImpl implements userService {
     @Override
     public String sayHi() {
-        return null;
+        return "hello dubbo";
     }
 }

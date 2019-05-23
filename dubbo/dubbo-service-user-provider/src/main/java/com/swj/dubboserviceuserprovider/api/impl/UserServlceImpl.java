10ff8e6b10ca08ba8503520f9f5309aed5c97d2d
1056438464 <1056438464@qq.com>
2019-05-23 23:00:01 +0800

    负载均衡

--- dubbo/dubbo-service-user-provider/src/main/java/com/swj/dubboserviceuserprovider/api/impl/UserServlceImpl.java
+++ dubbo/dubbo-service-user-provider/src/main/java/com/swj/dubboserviceuserprovider/api/impl/UserServlceImpl.java
@@ -2,11 +2,15 @@ package com.swj.dubboserviceuserprovider.api.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.swj.dubbo.service.user.api.userService;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service(version = "1.0.0")
 public class UserServlceImpl implements userService {
+
+    @Value("${dubbo.protocol.port}")
+    private String post;
     @Override
     public String sayHi() {
-        return "hello dubbo";
+        return "hello dubbo post is"+post;
     }
 }

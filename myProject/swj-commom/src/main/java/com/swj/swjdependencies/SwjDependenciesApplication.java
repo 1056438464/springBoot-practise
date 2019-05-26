edff082070929a1f856f741cd2d78fe5be324d92
jwsssssssss <1056438464@qq.com>
2019-05-26 22:50:03 +0800

    new project

---
+++ myProject/swj-commom/src/main/java/com/swj/swjdependencies/SwjDependenciesApplication.java
@@ -0,0 +1,13 @@
+package com.swj.swjdependencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwjDependenciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwjDependenciesApplication.class, args);
+	}
+
+}
---
+++ myProject/swj-commom/src/test/java/com/swj/swjdependencies/SwjDependenciesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.swj.swjdependencies;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SwjDependenciesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

a4d5c15bcd1dc18dff5d7a3098aebae5a85e825c
jwsssssssss <1056438464@qq.com>
2019-06-05 23:46:31 +0800

    dubbo provider

--- myProject/swj-service-user-provider/src/main/java/com/swj/service/user/provider/api/impl/TbUserServiceImpl.java
+++ myProject/swj-service-user-provider/src/main/java/com/swj/service/user/provider/api/impl/TbUserServiceImpl.java
@@ -2,19 +2,19 @@ package com.swj.service.user.provider.api.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.swj.commons.domain.SysUser;
+
 import com.swj.commons.mapper.SysUserMapper;
 import com.swj.service.user.provider.api.TbUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.annotation.Transient;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 @Service(version = "${services.versions.user.v1}")
 @Transactional(readOnly = true)
 public class TbUserServiceImpl implements TbUserService {
-    @Resource
+    @Autowired
     private SysUserMapper sysUserMapper;
     @Override
     public List<SysUser> selectAll() {

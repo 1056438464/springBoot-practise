2a483aeb3a96dd915d5c2d3accbc7b687e68db4b
jwsssssssss <1056438464@qq.com>
2019-06-15 15:37:55 +0800

    fastdfs

---
+++ myProject/myProject-service-upload/src/main/java/com/swj/myProject/service/upload/MyProjectServiceUploadApplication.java
@@ -0,0 +1,11 @@
+package com.swj.myProject.service.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectServiceUploadApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyProjectServiceUploadApplication.class, args);
+    }
+}

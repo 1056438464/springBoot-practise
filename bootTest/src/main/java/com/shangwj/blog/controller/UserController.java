cf4cf21968bb9a08d9e78683cacc48ccc9910d7a
jwsssssssss <1056438464@qq.com>
2019-08-08 23:27:09 +0800

    test

--- bootTest/src/main/java/com/shangwj/blog/controller/UserController.java
+++ bootTest/src/main/java/com/shangwj/blog/controller/UserController.java
@@ -1,43 +1,33 @@
 package com.shangwj.blog.controller;
 
-import com.shangwj.blog.model.Sysuser;
-import com.shangwj.blog.service.SysUserService;
+import com.shangwj.blog.mapper.impl.MenuMapper;
+import com.shangwj.blog.model.Menu;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.SetOperations;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import redis.clients.jedis.JedisCluster;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import static java.awt.SystemColor.info;
 
 
 @RestController
-@RequestMapping(value = "/myblog")
+@RequestMapping(value = "/test")
 public class UserController {
 
-    @Resource
-    private SysUserService sysUserService;
-
-
-
+    @Autowired
+    private MenuMapper menuMapper;
 
     JSONObject object = new JSONObject();
 
-
     @RequestMapping(value = "/getUserInfo")
     public String getUserInfo() {
-        List<Map<String,Object>> info = new ArrayList<>();
+        List<Menu> info = new ArrayList<>();
 
         try {
-            info = sysUserService.getAllUserInfo();
+            info = menuMapper.findAllRecursion();
             object.put("message", info);
         } catch (Exception e) {
             e.printStackTrace();
@@ -45,50 +35,4 @@ public class UserController {
         return object.toString();
     }
 
-
-    @RequestMapping(value = "/operaRedis")
-    public Object operaRedis() {
-
-        try {
-            sysUserService.operaRedisSet();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return object.put("message", "successful");
-    }
-
-    @RequestMapping(value = "/operaRedisInteger")
-    public Object operaRedisInteger() {
-
-        try {
-            sysUserService.operaRedisSetInteger();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return object.put("message", "successful");
-    }
-
-    @RequestMapping(value = "/operaRedisHash")
-    public Object operaRedisHash() {
-
-        try {
-            sysUserService.operaRedisHash();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return object.put("message", "successful");
-    }
-
-
-    @RequestMapping(value = "/operaRedisList")
-    public Object operaRedisList() {
-
-        try {
-            //redis
-            sysUserService.operaRedisList();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return object.put("message", "successful");
-    }
 }
